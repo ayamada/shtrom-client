df17db1e64a61e62f3344d284cee279e856ee3fc
Takashi AOKI <federkasten@me.com>
2014-06-24 20:22:48 +0900

    fix dependencies

--- project.clj
+++ project.clj
@@ -4,8 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/tools.logging "0.2.6"]
-                 [aleph "0.3.1-custom-SNAPSHOT"]]
+                 [org.clojure/tools.logging "0.3.0"]
+                 [aleph "0.3.2"]]
   :plugins [[lein-midje "3.1.3"]]
   :profiles {:dev {:dependencies [[midje "1.6.3"]]}}
   :jar-exclusions [#".+?\.config\.clj"])
