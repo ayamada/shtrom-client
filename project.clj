5eeb772e96167546ae56583ff03483f06bb48cc4
atsuo yamada <yamada-git@tir.jp>
2016-02-08 20:38:11 +0900

    Add tests for shtrom.client

--- project.clj
+++ project.clj
@@ -6,7 +6,8 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/tools.logging "0.3.1"]
                  [clj-http-lite "0.3.0"]]
-  :profiles {:test {:dependencies [[midje "1.8.3"]]
+  :profiles {:test {:resource-paths ["test-resources"]
+                    :dependencies [[midje "1.8.3"]]
                     :plugins [[lein-cloverage "1.0.6"]
                               [lein-midje "3.2"]]}}
   :jar-exclusions [#".+?\.config\.clj"])
--- test/shtrom/t_client.clj
+++ test/shtrom/t_client.clj
@@ -1,3 +1,36 @@
-(ns shtrom.t-client)
+(ns shtrom.t-client
+  (:require [midje.sweet :refer :all]
+            [shtrom.client :as client])
+  (:import [java.io StringWriter]))
 
-;;; These tests were moved to https://github.com/chrovis/shtrom
+(fact "validate-position"
+  (client/validate-position nil) => (throws Throwable)
+  (client/validate-position 0) => 0
+  (client/validate-position 1) => 1
+  (client/validate-position -1) => 0
+  (client/validate-position 99999) => 99999
+  (client/validate-position -99999) => 0
+  (instance? Long (client/validate-position 123)) => truthy)
+
+(fact "shtrom-init"
+  (client/shtrom-init "test.config.clj") => anything
+  client/host => "localhost"
+  client/port => 13001
+  client/uri-root => "http://localhost:13001")
+
+(with-state-changes [(before :facts (client/shtrom-init "test.config.clj"))]
+  (fact "hist-uri"
+    (client/hist-uri "test-key") => "http://localhost:13001/test-key"
+    (client/hist-uri "test-key" "test-ref" 64) => "http://localhost:13001/test-key/test-ref/64")
+  (fact "load-hist"
+    (client/load-hist "test-key" "test-ref" 64 0 128) => nil)
+  (fact "save-hist"
+    (client/save-hist "test-key" "test-ref" 64 []) => (throws RuntimeException)
+    (client/save-hist "test-key" "test-ref" 64 [0 1 2 3]) => nil)
+  (fact "reduce-hist"
+    (client/reduce-hist "test-key" "test-ref" 64) => nil)
+  (fact "delete-hist"
+    (client/delete-hist "test-key") => nil))
+
+;;; More tests (must need shtrom server) were moved to
+;;; https://github.com/chrovis/shtrom
