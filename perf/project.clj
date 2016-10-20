c5155be30f6f88ab359a0c3959c20b9bdab7f481
atsuo yamada <yamada-git@tir.jp>
2016-10-21 01:50:35 +0900

    Add perf tools

---
+++ perf/project.clj
@@ -0,0 +1,7 @@
+(defproject shtrom-client-performance "0.1.0-SNAPSHOT"
+  :description "Performance test of shtrom"
+  :license {:name "Apache License, Version 2.0"
+            :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [shtrom-client "0.1.0-SNAPSHOT"]
+                 [com.climate/claypoole "1.1.2"]])
